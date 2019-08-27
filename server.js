81afd50de407cd2312b5dd4af74ab894aff7dbeb
Maryam Seyedi <maryams.say@gmail.com>
2019-08-27 22:02:07 +0200

    addidng port

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.put('/image', (req, res)=>{
     .catch(err => res.status(400).json('unable to get entreis'))
 })
 
-app.listen(3000, ()=>{
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
