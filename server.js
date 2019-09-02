a7fcbd782afdc0c324c8332a6705e157ebd0ca50
Maryam Seyedi <maryams.say@gmail.com>
2019-09-02 13:55:36 +0200

    introspection

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const db =knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl:true
+      ssl:true,
+      introspection : true
     }
   });
 
